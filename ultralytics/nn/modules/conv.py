a848339eb05d911f505e57d8b6a2397865dd3ea1
Rajasripuvi <rajasri.k171@ptuniv.edu.in>
2025-04-11 16:13:29 +0530

    Update conv.py
    elu
    
    Signed-off-by: Rajasripuvi <rajasri.k171@ptuniv.edu.in>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -45,7 +45,7 @@ class Conv(nn.Module):
         default_act (nn.Module): Default activation function (SiLU).
     """
 
-    default_act = nn.SiLU()  # default activation
+   default_act = nn.ELU(inplace=True)  # Switched from SiLU to ELU
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """
@@ -65,7 +65,6 @@ class Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-
     def forward(self, x):
         """
         Apply convolution, batch normalization and activation to input tensor.
